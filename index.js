bf9b1c59be731b94af30772976f76361b0bd0432
Federico Menozzi <federicogmenozzi@gmail.com>
2017-05-04 17:19:40 -0400

    Starting to look legit

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ $(function() {
             $("#about").show();
             $("#projects").hide();
             $("#blog").hide();
+
+            $(".sidebar-links a").removeClass("sidebar-active");
+            $("[href='#/about']").addClass("sidebar-active");
         },
 
         "/projects": function() {
             $("#about").hide();
             $("#projects").show();
             $("#blog").hide();
+
+            $(".sidebar-links a").removeClass("sidebar-active");
+            $("[href='#/projects']").addClass("sidebar-active");
         },
 
         "/blog": function() {
             $("#about").hide();
             $("#projects").hide();
             $("#blog").show();
+
+            $(".sidebar-links a").removeClass("sidebar-active");
+            $("[href='#/blog']").addClass("sidebar-active");
         },
     };
 
