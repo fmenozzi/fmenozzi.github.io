988907ce7987260639b29104fd317b9c4138a387
Federico Menozzi <federicogmenozzi@gmail.com>
2017-05-08 21:45:38 -0400

    No blog (for now)

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $(function() {
         "/about": function() {
             $("#about").show();
             $("#projects").hide();
-            $("#blog").hide();
 
             $(".sidebar-links a").removeClass("sidebar-active");
             $("[href='#/about']").addClass("sidebar-active");
@@ -12,19 +11,9 @@ $(function() {
         "/projects": function() {
             $("#about").hide();
             $("#projects").show();
-            $("#blog").hide();
 
             $(".sidebar-links a").removeClass("sidebar-active");
             $("[href='#/projects']").addClass("sidebar-active");
-        },
-
-        "/blog": function() {
-            $("#about").hide();
-            $("#projects").hide();
-            $("#blog").show();
-
-            $(".sidebar-links a").removeClass("sidebar-active");
-            $("[href='#/blog']").addClass("sidebar-active");
         },
     };
 
