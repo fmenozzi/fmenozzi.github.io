8ddf081bbd6b302530ac0de47fad4358f3728d68
Federico Menozzi <federicogmenozzi@gmail.com>
2017-05-24 16:12:06 -0400

    Nice mobile sidebar/hamburger menu!

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ $(function() {
             $("#about").show();
             $("#projects").hide();
 
+            $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
+
             $(".sidebar-links a").removeClass("sidebar-active");
             $("[href='#/about']").addClass("sidebar-active");
         },
@@ -12,10 +14,20 @@ $(function() {
             $("#about").hide();
             $("#projects").show();
 
+            $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
+
             $(".sidebar-links a").removeClass("sidebar-active");
             $("[href='#/projects']").addClass("sidebar-active");
         },
     };
 
     Router(routes).init("/about");
+
+    $("#navbar-button").click(function() {
+        $("#sidebar, #mobile-navbar-bogus-buffer").toggleClass("sidebar-visible");
+    });
+
+    $("#mobile-navbar-bogus-buffer").click(function() {
+        $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
+    })
 });
