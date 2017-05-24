2d33fd1f684279f052661e824f12a0e06735ac1a
Federico Menozzi <federicogmenozzi@gmail.com>
2017-05-24 17:30:31 -0400

    Add home section

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 $(function() {
     let routes = {
+        "/": function() {
+            $("#home").show();
+            $("#about").hide();
+            $("#projects").hide();
+
+            $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
+
+            $(".sidebar-links a").removeClass("sidebar-active");
+            $(".sidebar-links a[href='#/']").addClass("sidebar-active");
+        },
+
         "/about": function() {
+            $("#home").hide();
             $("#about").show();
             $("#projects").hide();
 
             $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
 
             $(".sidebar-links a").removeClass("sidebar-active");
-            $("[href='#/about']").addClass("sidebar-active");
+            $(".sidebar-links a[href='#/about']").addClass("sidebar-active");
         },
 
         "/projects": function() {
+            $("#home").hide();
             $("#about").hide();
             $("#projects").show();
 
             $("#sidebar, #mobile-navbar-bogus-buffer").removeClass("sidebar-visible");
 
             $(".sidebar-links a").removeClass("sidebar-active");
-            $("[href='#/projects']").addClass("sidebar-active");
+            $(".sidebar-links a[href='#/projects']").addClass("sidebar-active");
         },
     };
 
-    Router(routes).init("/about");
+    Router(routes).init("#/");
 
     $("#navbar-button").click(function() {
         $("#sidebar, #mobile-navbar-bogus-buffer").toggleClass("sidebar-visible");
